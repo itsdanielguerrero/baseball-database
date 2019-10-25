cb4d1ef57029c24a9796452fc528d7fe15b45f0a
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-25 05:26:12 -0400

    pushing baseball database project

---
+++ creates.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE baseball
+
+USE baseball
+
+CREATE TABLE players (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    
+);
