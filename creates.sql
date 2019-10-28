bef9932947a95cf58f17a084bbf6ecdd9bf0ad43
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-28 19:26:42 -0400

    in progress

--- creates.sql
+++ creates.sql
@@ -1,10 +1,13 @@
-CREATE DATABASE baseball
+CREATE DATABASE baseball;
 
-USE baseball
+USE baseball;
 
 CREATE TABLE players (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    
+    throws VARCHAR(1) NOT NULL,
+    hits VARCHAR(1) NOT NULL,
+    PRIMARY KEY (ID)
 );
+
